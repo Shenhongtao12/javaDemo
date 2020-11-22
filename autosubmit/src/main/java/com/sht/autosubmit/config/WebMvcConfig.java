1b6adfd1033965caf45d77fa9f964807d902b5a6
Shenhongtao12 <shenhongtao12@aliyun.com>
2020-11-22 22:10:37 +0800

    处理option请求

--- autosubmit/src/main/java/com/sht/autosubmit/config/WebMvcConfig.java
+++ autosubmit/src/main/java/com/sht/autosubmit/config/WebMvcConfig.java
@@ -3,14 +3,26 @@ package com.sht.autosubmit.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
 
 /**
  * @author Aaron
  */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    private CorsInterceptor corsInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 跨域拦截器需放在最上面
+        registry.addInterceptor(corsInterceptor).addPathPatterns("/**");
+    }
 
     /**
      * 跨域处理
@@ -20,6 +32,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addMapping("/")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH")
+                //允许任何请求头
+                .allowedHeaders("*")
                 .allowCredentials(true).maxAge(3600);
     }
 
