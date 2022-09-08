e02366e15e502093bd00a07da1d44f1c5c1a1244
Aaron Shen <Aaron.H.Shen@newegg.com>
2022-09-08 18:38:57 +0800

    💪🚩

---
+++ common/src/test/java/com/sht/common/Test.java
@@ -0,0 +1,17 @@
+package com.sht.common;
+
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<Integer>();
+        System.out.println(System.currentTimeMillis());
+
+    }
+}
