453c6770fe04b7abdd32b7f4380f72a1ebd9a085
Aaron Shen <Aaron.H.Shen@newegg.com>
2022-11-19 17:43:31 +0800

    分片上传

---
+++ breakpoint-http/src/test/java/com/sht/common/CommonApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sht.common;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
