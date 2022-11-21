f900f8b567ea9cad6babaac6707b9aa5325e5734
Aaron <53165382+Shenhongtao12@users.noreply.github.com>
2022-11-21 22:44:55 +0800

    优化

--- breakpoint-http/src/main/java/com/sht/common/Application.java
+++ breakpoint-http/src/main/java/com/sht/common/Application.java
@@ -1,16 +1,9 @@
 package com.sht.common;
 
-import com.sht.common.service.StorageService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
-@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
